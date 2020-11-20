5a6cebed361b7a673d60e3c28f2058f984b7c38a
lijilong5201314 <2413045612@qq.com>
2020-11-20 21:47:23 +0800

    11月18日以及19日的作业

---
+++ 11_19.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	/*11月20日第二题int a, b, c;
+	int tmp;
+	printf("请输入三个数：\n");
+	scanf("%d,%d,%d", &a, &b, &c);
+	if (a<b)
+	{
+		tmp = a;
+		a = b;
+		b = tmp;
+	}
+	if (a<c)
+	{
+		tmp = a;
+		a = c;
+		c = tmp;
+	}
+	if (b<c)
+	{
+		tmp = b;
+		b = c;
+		c = tmp;
+	}
+	printf("%d %d %d\n", a, b, c);
+	return 0;*/
+
+	//11月20日第二题int i = 1;
+	//while (i <= 100)
+	//{
+	//	if (i % 3 == 0)
+	//		printf("%d ", i);
+	//	i++;
+	//}
+	//return 0;
+
+//	/*11月8日int a , b;
+//	scanf("%d%d", &a, &b);
+//	printf("%d", MAX(a, b));*/
+//}
+//int MAX(int x, int y)
+//{
+//	int max;
+//	if (x > y)
+//	{
+//		max = x;
+//	}
+//	else
+//	{
+//		max = y;
+//	}
+//	return max;
+//}
